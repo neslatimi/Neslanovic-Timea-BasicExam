32389895169dcc7184f959444e6084862df59e52
neslanovic <timy1901@gmail.com>
2019-07-19 16:19:42 +0200

    js functions are done

--- js/got.js
+++ js/got.js
@@ -25,22 +25,22 @@ const got = {
   showAllInMain() {
     let gotTemplate = '';
     this.actors.forEach((element, i) => {
-      gotTemplate += `<div data-id="${i}" classs="main_container_dives">
+      gotTemplate += `<div class="main_container_dives">
               <img src="/${this.actors[i].portrait}" alt="${this.actors[i].name}">
-              <div class="got_name" onclick="got.showDescription()" data-nameIndex="${i}">${this.actors[i].name}</div>
+              <div class="got_name" onclick="got.showDescription(${i})">${this.actors[i].name}</div>
               </div>`;
       document.querySelector('.main_container').innerHTML = gotTemplate;
     });
   },
-  showDescription() {
-    const gotTargetIndex = event.target.dataset.nameindex;
+  showDescription(gotTargetIndex) {
+    // const gotTargetIndex = event.target.dataset.nameindex;
     const createDisriptionDiv = `<div class="aside_container">
          <div class="aside_container-picture"> <img src="/${this.actors[gotTargetIndex].picture}" 
           alt="${this.actors[gotTargetIndex].name} picture">
           <div class="aside_container-name">${this.actors[gotTargetIndex].name}${this.isLogoExist(gotTargetIndex)}</div>
           <div class="aside_container-description">${this.actors[gotTargetIndex].bio}</div>
     </div>`;
-    document.querySelector('.aside').innerHTML = createDisriptionDiv;
+    document.querySelector('.main_container-description').innerHTML = createDisriptionDiv;
   },
   isLogoExist(index) {
     if (this.actors[index].house === undefined) {
@@ -51,6 +51,23 @@ const got = {
   alt="${this.actors[index].name} house">`;
     }
   },
-
+  searchByName() {
+    const inputValue = document.querySelector('.input_search');
+    for (let i = 0; i < got.actors.length; i += 1) {
+      if (got.actors[i].name.toLowerCase() === inputValue.value.toLowerCase()) {
+        inputValue.value = '';
+        return this.showDescription(i);
+      }
+    }
+    inputValue.value = '';
+    return this.showNotFound();
+  },
+  showNotFound() {
+    const notFound = document.querySelector('.notFound--display-none');
+    notFound.classList.remove('notFound--display-none');
+    setTimeout(() => {
+      notFound.classList.add('notFound--display-none');
+    }, 5000);
+  },
 };
 got.init();
